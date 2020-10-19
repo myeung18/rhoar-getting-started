039871f7898916b53e3cfff3c8dc32b929e275d3
Marco Yeung <myeung@redhat.com>
2020-10-19 13:28:20 -0400

    local test connect db

--- nodejs/app.js
+++ nodejs/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 let configMap;
-let message = "Default hard-coded greeting: Hello, %s!";
+let message = "Default hard-coded greeting: Hello-hey, %s!";
 
 app.use('/api/greeting', (request, response) => {
   const name = (request.query && request.query.name) ? request.query.name : 'World';
@@ -26,7 +26,39 @@ app.use('/api/greeting', (request, response) => {
 probe(app);
 
 // TODO: Periodic check for config map update
+setInterval(() => {
+  displayMessage().then((config) => {
+    if (!config) {
+      message = null;
+      return;
+    }
+
+    if (JSON.stringify(config) !== JSON.stringify(configMap)) {
+      configMap = config;
+      message = config.message;
+    }
+  }).catch((err) => {
+    console.log("error: ", err);
+  });
+}, 2000);
 
 // TODO: Retrieve ConfigMap
+// Find the Config Map
 
+//const openshiftRestClient = require('openshift-rest-client').OpenshiftClient;
+//const config = require('openshift-rest-client').config;
+//const customConfig = config.getInCluster();
+//
+//async function retrieveConfigMap() {
+//  const client = await openshiftRestClient({config: customConfig});
+//  const configmap = await client.api.v1.namespaces('example').configmaps('app-config').get();
+//  return jsyaml.safeLoad(configmap.body.data['app-config.yml']);
+//}
+
+async function displayMessage() {
+    console.log("hello fron concole.")
+}
+
+
+module.exports = app;
 module.exports = app;
